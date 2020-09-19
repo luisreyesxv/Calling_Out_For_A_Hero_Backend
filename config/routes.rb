d437063786cfca06f6babce3d5dcfa4880d93bc6
Luis Reyes <luisreyesxv@yahoo.com>
2020-09-19 05:18:09 -0400

    small change

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :tasks, except: [:show]
-  resources :chosen_heros
+  resources :chosen_heros #need to finish breaking this one down, was probably going to remove index, and make it give a custom random 3? chosen_heroes
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   post '/register', to: 'users#register'
