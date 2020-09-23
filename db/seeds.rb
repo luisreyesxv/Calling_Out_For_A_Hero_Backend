84f56abaddb688f2eb42019b160bf599920dbe3b
Luis Reyes <luisreyesxv@yahoo.com>
2020-09-23 03:13:24 -0400

    started to set up the sprite sheet seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,8 +13,8 @@ Hero.destroy_all
 
 
 
-u1=User.create(name: "luis",email: "fake@email.com",password: "totallyfake")
-u2=User.create(name: "luis2",email: "fake2@email.com",password: "totallyfake")
+u1=User.create(name: "testing 1",email: "fake@email.com",password: "totallyfake")
+u2=User.create(name: "luis",email: "luis2@email.com",password: "totallyfake")
 u3=User.create(name: "luis3",email: "fake3@email.com",password: "totallyfake")
 
 t1=Task.create(user: u1,title:"title1",description:"testing random description",date: Time.now())
@@ -24,16 +24,16 @@ t4=Task.create(user: u1,title:"title4",description:"testing random description",
 t5=Task.create(user: User.all.sample,title:"title5",description:"testing random description",date: Time.now())
 
 
-h1= Hero.create(main_attribute:"Country")
-h2= Hero.create(main_attribute:"Rap")
+h1= Hero.create(main_attribute:"Country", width: 816.80, height: 593.50)
+h2= Hero.create(main_attribute:"Rap", width: 740, height: 805.66)
 h3= Hero.create(main_attribute:"Pop")
 h4= Hero.create(main_attribute:"Swing")
 
-h1.sprite.attach(io: File.open('/mnt/c/Users/Admin/Desktop/Projects/Mod 5/Pitch/3_3-PIRATE_HURT_006.png'), filename: 'pirate.png', content_type: 'image/png')
+h1.sprite.attach(io: File.open('/mnt/c/Users/Admin/Desktop/Projects/Mod 5/Sprites and Backgrounds/Elves/1/sheet pieces/cropped sprite sheet.png'), filename: 'elf 1.png', content_type: 'image/png')
 puts "put h1 sprite"
-h2.sprite.attach(io: File.open('/mnt/c/Users/Admin/Desktop/Projects/Mod 5/Pitch/Fairy_02__JUMP_006.png'), filename: 'fairy1.png', content_type: 'image/png')
+h2.sprite.attach(io: File.open('/mnt/c/Users/Admin/Desktop/Projects/Mod 5/Sprites and Backgrounds/Fairies/3/sheet pieces/spritesheet.png'), filename: 'Dragonfly Peppershimmer.png', content_type: 'image/png')
 puts "put h2 sprite"
-h3.sprite.attach(io: File.open('/mnt/c/Users/Admin/Desktop/Projects/Mod 5/Pitch/Fairy_03__WALK_006.png'), filename: 'fairy2.png', content_type: 'image/png')
+h3.sprite.attach(io: File.open('mnt/c/Users/Admin/Desktop/Projects/Mod 5/Sprites and Backgrounds/Woman Warriors/2/sheet pieces/spritesheet.png'), filename: 'Freya Wilddottir.png', content_type: 'image/png')
 puts "put h3 sprite"
 h4.sprite.attach(io: File.open('/mnt/c/Users/Admin/Desktop/Projects/Mod 5/Pitch/Knight_01__ATTACK_008.png'), filename: 'knight.png', content_type: 'image/png')
 puts "put h4 sprite"
@@ -44,8 +44,8 @@ puts "put h4 sprite"
 
 
 
-c1 = ChosenHero.create(user:u1, hero:h1, name: "monster 1", reputation:34)
+c1 = ChosenHero.create(user:u1, hero:h1, name: "Elfberto", reputation:34)
 puts "chosen hero id is #{c1.id}"
-c2 = ChosenHero.create(user:u2, hero:h4, name: "monster 31", reputation:14)
-c3 = ChosenHero.create(user:User.all.sample, hero:Hero.all.sample, name: "monster 2", reputation:31)
-c4 = ChosenHero.create(user:u3, hero:Hero.all.sample, name: "monster 441", reputation:54)
+c2 = ChosenHero.create(user:u2, hero:h4, name: "Dread Pirate Roberto", reputation:14)
+c3 = ChosenHero.create(user:u4, hero:h2, name: "Dragonfly Peppershimmer", reputation:31)
+c4 = ChosenHero.create(user:u3, hero:h3, name: "Freya Wilddottir", reputation:54)
